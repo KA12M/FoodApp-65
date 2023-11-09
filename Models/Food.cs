18d54c5e8e3367879a8e73dc27981810549ce8d5
Karan Khumthong <80217497+KA12M@users.noreply.github.com>
2023-11-09 15:43:50 +0700

    Update Food.cs

--- Models/Food.cs
+++ Models/Food.cs
@@ -1,13 +1,12 @@
 
-namespace Models.FoodApp_65
+namespace Models.FoodApp_65;
+
+public class Food
 {
-    public class Food
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Type { get; set; }
-        public double Cost { get; set; }
-        public double Cal { get; set; }
-        public string CostRate { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Type { get; set; }
+    public double Cost { get; set; }
+    public double Cal { get; set; }
+    public string CostRate { get; set; }
 }
